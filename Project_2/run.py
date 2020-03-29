0ce6035fc4862ea80a7af0229e3aee23f4d1d725
Yousef Elsendiony <YousefElsendiony@gmail.com>
2020-03-29 09:19:41 -0600

    Mainly working. Needs better printing and requery with highest relation

--- Project_2/run.py
+++ Project_2/run.py
@@ -4,13 +4,21 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from stanfordnlp.server import CoreNLPClient
+from stanfordnlp.server import to_text
+
+extractedTuples = set()
+visitedURLs = set()
+sentenceNers = set()
 
 relations = {
-1: "Schools_Attended",
-2: "Work_For",
-3: "Live_In",
-4: "Top_Member_Employees"
+1: "per:schools_attended", #Schools_Attended
+2: "per:employee_or_member_of",#Work_For
+3: "per:cities_of_residence",#Live_In
+4: "org:top_members_employees"#Top_Member_Employees
 }
+
+nersRelationtype1 = ["ORGANIZATION","PERSON"]
+nersRelationtype2 = ["LOCATION","CITY", "STATE_OR_PROVINCE", "COUNTRY"]
 
 annotators_ner = ['tokenize','ssplit','pos','lemma','ner']
 annotators_kbp = ['tokenize','ssplit','pos','lemma','ner','depparse','coref','kbp']
@@ -24,23 +32,45 @@ def tag_visible(element):
     return True
 
 
+
+def checkKBPConfidence(ann_kbp, counterExtractedTuples):
+    # check confidence > threshold
+    for sentence in ann_kbp.sentence:
+        for kbp_triple in sentence.kbpTriple:
+            if kbp_triple.confidence > t:
+                print("\t=== Extracted Relation ===")
+                print("\tSentence: ", to_text(sentence))
+                print(f"\tConfidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}")
+                extractedTuples.add((kbp_triple.confidence,str(kbp_triple.subject)+","+str(kbp_triple.object)))
+                print("\tAdding to set of extracted relations")
+                print("\t==========")
+                counterExtractedTuples +=1
+
+            else:
+                print("\t=== Extracted Relation ===")
+                print("\tSentence: ", to_text(sentence))
+                print(f"\tConfidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}")
+                print("\tConfidence is lower than threshold confidence. Ignoring this.")
+                print("\t==========")
+    return counterExtractedTuples
+
 def main(api_key, engine_id, r, t, q, k):
-    extractedTuples = 0
     queryIteration = 0
     pageNumberVisited = 0
     r = relations[r]
+    counterExtractedTuples = 0
 
     # Initial Print of Parameters
     print("\nParameters:")
     print("Client key   = ", api_key)
     print("Engine key   = ", engine_id)
     print("Relation     = ", r)
-    print("Threshold    = ", api_key)
+    print("Threshold    = ", t)
     print("Query        = ", q)
     print("# of Tuples  = ", k)
     print("Loading necessary libraries; This should take a minute or so ...")
 
-    while (extractedTuples < k) and (queryIteration < 9) :
+    while (len(extractedTuples) < k) and (queryIteration < 9) :
         print("=========== Iteration: %s - Query: %s ===========" % (queryIteration, q))
 
         # Google search API (returns top 10 pages)
@@ -56,15 +86,24 @@ def main(api_key, engine_id, r, t, q, k):
         for page in res['items']:
 
             # Print URL and mark as visited
-            pageNumberVisited+=1
             url = page['formattedUrl']
+            if(url in visitedURLs):
+                print("Already seen URL ... skipping")
+                continue;
+            else:
+                visitedURLs.add(url)
+            pageNumberVisited+=1
             print(("URL (%s / 10): " + url) % pageNumberVisited )
 
 
 
             # Get the 20000 characters from page
             print("Fetching text from url ...")
-            rawPage = requests.get(url)
+            try:
+                rawPage = requests.get(url)
+            except:
+                print("Unable to fetch URL. Continuing.")
+                break
             contents = BeautifulSoup(rawPage.text, 'html.parser')
             pageText = contents.findAll(text=True)
             rawText = filter(tag_visible, pageText)
@@ -79,16 +118,50 @@ def main(api_key, engine_id, r, t, q, k):
             # Do the annotation
             print("Annotating the webpage using [tokenize, ssplit, pos, lemma, ner] annotators ...")
 
-            with CoreNLPClient(timeout=30000, memory='4G', be_quiet=False) as pipeline:
-                for j in range(100):
-                    print(f">>> Repeating {j}th time.")
-                    ann_ner = pipeline.annotate(rawText, annotators=annotators_ner)
-                    ann_kbp = pipeline.annotate(rawText, annotators=annotators_kbp)
-                    for sentence in ann_kbp.sentence:
-                        for kbp_triple in sentence.kbpTriple:
-                            print(f"\t Confidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}")
 
+            counterLastIterationExtractedTuples = counterExtractedTuples
 
+            # TA PROVIDED CODE For KBP TRIPLE EXTRACTION
+            with CoreNLPClient(timeout=30000, memory='4G', be_quiet=True) as pipeline:
+                ann_ner = pipeline.annotate(rawText, annotators=annotators_ner)
+                # print 330 lines
+
+                # check ner tags  ( CHECK THE RELATION HERE )
+                for sentence in ann_ner.sentence:
+                    sentenceText = to_text(sentence)
+                    #print("Sentence: ", sentenceText)
+
+                    for token in sentence.token:
+                        #print(f"****Token word:: {token.word};\t ner : {token.ner}; ")
+                        if (token.ner not in sentenceNers):
+                            if r == "per:cities_of_residence":
+                                if(token.ner in nersRelationtype2):
+                                    sentenceNers.add(token.ner)
+                            else:
+                                if(token.ner in nersRelationtype1):
+                                    sentenceNers.add(token.ner)
+                    #print("Sentence NERS: ", sentenceNers)
+
+                    if r == "per:cities_of_residence":
+                        if (nersRelationtype2[0] in sentenceNers) and (nersRelationtype2[1] in sentenceNers) \
+                            or (nersRelationtype2[2] in sentenceNers) or (nersRelationtype2[3] in sentenceNers):
+                            # KBP Annotate for more detailed analysis
+                            ann_kbp = pipeline.annotate(sentenceText, annotators=annotators_kbp)
+                            counterExtractedTuples = checkKBPConfidence(ann_kbp, counterExtractedTuples)
+                        else:
+                            #print("~~~~~~NO MATCHING NERS")
+                            pass
+                    else:
+                        if nersRelationtype1[0] in sentenceNers and nersRelationtype1[1] in sentenceNers:
+                            # KBP Annotate for more detailed analysis
+                            ann_kbp = pipeline.annotate(sentenceText, annotators=annotators_kbp)
+                            counterExtractedTuples = checkKBPConfidence(ann_kbp, counterExtractedTuples)
+                        else:
+                            #print("~~~~~~NO MATCHING NERS")
+                            pass
+                    sentenceNers.clear()
+            # End of webPage
+            print(f"Relations extracted from this website {counterExtractedTuples - counterLastIterationExtractedTuples} (Overall: {counterExtractedTuples})")
         # Next iteration, need new query based off high confidence tuple
         queryIteration += 1
 
